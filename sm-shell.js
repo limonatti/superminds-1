bf97e210c7add5c4ce8ba176b2d7d950604dff28
limonatti <limonatti@gmail.com>
2026-08-11 08:16:21 +0300

    Vernut Konstruktor v menyu uchitelya

--- sm-shell.js
+++ sm-shell.js
@@ -19,7 +19,8 @@ var I = {
   sound: '<path d="M11 5L6 9H2v6h4l5 4z"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14"/><path d="M15.54 8.46a5 5 0 0 1 0 7.07"/>',
   check: '<path d="M20 6L9 17l-5-5"/>',
   clock: '<circle cx="12" cy="12" r="10"/><path d="M12 6v6l4 2"/>',
-  chat:  '<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>'
+  chat:  '<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>',
+build: '<path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/>'
 };
 
 function svg(name, size) {
@@ -46,6 +47,7 @@ var STUDENT = [
 ];
 var TEACHER = [
   { id: "class",    t: "Мой класс",        i: "users", href: "teacher-class.html" },
+{ id: "builder",  t: "Конструктор",      i: "build", href: "builder.html" },
   { id: "review",   t: "Проверка домашки", i: "file",  href: "teacher-homework.html" },
   { id: "chat",     t: "Сообщения",        i: "chat",  href: "chat.html" },
   { id: "tsched",   t: "Расписание",       i: "cal",   href: "teacher-schedule.html" },
